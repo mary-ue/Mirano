4bc06be84287a4ea8a7dbc2c28f23fcca52fd601
mary-ue <maryandreeva100@gmail.com>
2024-05-13 14:39:25 +0300

    add filters

--- src/scripts/filterProducts.js
+++ src/scripts/filterProducts.js
@@ -1,18 +1,50 @@
 import { fetchProducts } from './api';
-
-const filterType = type => {
-  fetchProducts({ type: type.value });
-}
+import { debounce } from './debounce';
 
 export const filterProducts = () => {
   const filterForm = document.querySelector('.filter__form');
-  filterType(filterForm.type);
+  const goodsTitle = document.querySelector('.goods__title');
+
+  const applyFilters = () => {
+    const formData = new FormData(filterForm);
+    const type = formData.get('type');
+    const minPrice = formData.get('minPrice');
+    const maxPrice = formData.get('maxPrice');
+    const params = {};
+
+    if (type) {
+      params.type = type;
+    }
+
+    if (minPrice) {
+      params.minPrice = minPrice;
+    }
+
+    if (maxPrice) {
+      params.maxPrice = maxPrice;
+    }
+
+    fetchProducts(params);
+  };
+
+  applyFilters();
+
+  const applyPriceFilters = debounce(applyFilters, 300);
 
   filterForm.addEventListener('input', (evt) => {
     const target = evt.target;
 
     if (target.name === 'type') {
-      filterType(filterForm.type);
+      goodsTitle.textContent = target.labels[0].textContent;
+
+      filterForm.maxPrice.value = '';
+      filterForm.minPrice.value = '';
+      applyFilters();
+      return;
+    } 
+
+    if (target.name === 'minPrice' || target.name === 'maxPrice') {
+      applyPriceFilters();
     }
   });
 };
